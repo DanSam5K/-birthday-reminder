f1c163380f7ceff0c5be3c69e1177cb65d107567
Daniel Samuel <dansam5000@gmail.com>
2021-02-17 01:50:01 +0100

    files set-up and d data inputs

---
+++ birthdayapp/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Suzan Luka',
+    age: 25,
+    image:
+      'https://scontent.flos3-1.fna.fbcdn.net/v/t1.0-9/81410015_486244772275926_636673191611203584_o.jpg?_nc_cat=102&ccb=3&_nc_sid=09cbfe&_nc_eui2=AeH8qn4Tf6a7JjJvJ9g6ktQIiioL-IU-hbSKKgv4hT6FtAf9HmOyp5REoV5H64V2G6Cp6TyJaQI6GJ4t0NHYa80z&_nc_ohc=BZkJhfOgmR0AX-2PE9r&_nc_ht=scontent.flos3-1.fna&oh=02d06eeb8aa3eee81c287c84ef551a58&oe=60525BF2',
+  },
+  {
+    id: 2,
+    name: 'Isaac Emmanuel',
+    age: 32,
+    image:
+      'https://scontent.flos3-2.fna.fbcdn.net/v/t1.0-9/127504324_10218240012020704_6359420166985813336_o.jpg?_nc_cat=111&ccb=3&_nc_sid=09cbfe&_nc_eui2=AeHLnQSEiUTDAXduavVSVKoZ0skdVigRhj7SyR1WKBGGPgxFdwaagchsPOtNxVuPIXgY_DV2yNz6QzbU_s7rtciV&_nc_ohc=b-L24ycKvpAAX-uatvD&_nc_ht=scontent.flos3-2.fna&oh=bbca9fb379e2594564077ae2900cdaa3&oe=60508F70',
+  },
+  {
+    id: 3,
+    name: 'Zizoh Anto',
+    age: 30,
+    image:
+      'https://scontent.flos3-2.fna.fbcdn.net/v/t1.0-9/82106525_10221525319471621_3896887122593841152_o.jpg?_nc_cat=110&ccb=3&_nc_sid=09cbfe&_nc_eui2=AeE6oj7BMYxEQFPHAzuOU6KXcUjt8hErweNxSO3yESvB43VEOMxE6q2rtVSihZkh3Gw97p8SYD7eOK34PDOnRnIs&_nc_ohc=_0gqq81BifwAX8djmNF&_nc_ht=scontent.flos3-2.fna&oh=597fa8b2d448dade9c306e65c6c06abe&oe=6052F87E',
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
